2df1dd2553ca6dab75299185a515bb7d728bf6bc
kmx36 <jobmx@yandex.ru>
2025-01-04 17:27:11 +0300

    admin_zone

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Location, Post
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'is_published',
+        'created_at',
+        'title',
+        'description',
+        'slug',
+    )
+    search_fields = (
+        'title',
+        'description',
+        'slug',
+    )
+    list_filter = (
+        'is_published',
+    )
+    list_display_links = (
+        'title',
+    )
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'is_published',
+        'created_at',
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'is_published',
+    )
+    list_display_links = (
+        'name',
+    )
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'is_published',
+        'created_at',
+        'title',
+        'text',
+        'pub_date',
+        'author'
+    )
+    search_fields = (
+        'title',
+        'text',
+    )
+    list_filter = (
+        'is_published',
+        'author',
+        'location',
+        'category',
+    )
+    list_display_links = (
+        'title',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Location, LocationAdmin)
+
+admin.site.register(Post, PostAdmin)
+
+admin.site.empty_value_display = 'Не задано'
