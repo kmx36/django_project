9c7356af2da82e3ed8040f130b27b92c0746a65c
kmx36 <jobmx@yandex.ru>
2025-01-08 19:06:46 +0300

    #2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,29 +5,34 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class PublishedModel(models.Model):
+class BaseModel(models.Model):
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
-        help_text=('Снимите галочку, чтобы скрыть публикацию.')
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      verbose_name='Добавлено')
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено',
+    )
 
     class Meta:
         abstract = True
 
 
-class Category(PublishedModel):
-    title = models.CharField(max_length=256, unique=True,
-                             verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
+class Category(BaseModel):
+    title = models.CharField(
+        max_length=256, verbose_name='Заголовок'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
     slug = models.SlugField(
-        max_length=256, unique=True, blank=False, verbose_name='Идентификатор',
-        help_text=(
-            'Идентификатор страницы для URL; '
-            'разрешены символы латиницы, цифры, дефис и подчёркивание.'
-        )
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text='Идентификатор страницы для URL; '
+        'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+
     )
 
     class Meta:
@@ -35,85 +40,73 @@ class Category(PublishedModel):
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return self.title[:20]
+        return self.title
 
 
-class Location(PublishedModel):
-    name = models.CharField(max_length=256, unique=True,
-                            verbose_name='Название места')
+class Location(BaseModel):
+    name = models.CharField(
+        max_length=256, verbose_name='Название места'
+    )
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
     def __str__(self):
-        return self.name[:20]
+        return self.name
 
 
-class Post(PublishedModel):
-    title = models.CharField(max_length=256, unique=True,
-                             verbose_name='Заголовок')
-    text = models.TextField(blank=False, verbose_name='Текст')
+class Post(BaseModel):
+    title = models.CharField(
+        max_length=256, verbose_name='Заголовок'
+    )
+    text = models.TextField(
+        verbose_name='Текст'
+    )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text=(
-            'Если установить дату и время в будущем — '
-            'можно делать отложенные публикации.'
-        )
+        help_text='Если установить дату и время в будущем — '
+        'можно делать отложенные публикации.'
     )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор публикации',
-        related_name='posts'
+        User, on_delete=models.CASCADE,
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
         Location,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        verbose_name='Местоположение',
-        related_name='posts'
+        on_delete=models.SET_NULL, null=True, blank=True,
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
-        on_delete=models.SET_NULL,
-        null=True, blank=False,
-        verbose_name='Категория',
-        related_name='posts'
+        on_delete=models.SET_NULL, null=True,
+        verbose_name='Категория'
     )
     image = models.ImageField(
-        'Фото',
-        upload_to='posts_images',
-        blank=True,
+        upload_to='blogs_images', null=True, blank=True,
+        verbose_name='Фото'
     )
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-        ordering = ('-pub_date',)
+        default_related_name = 'posts'
 
     def __str__(self):
-        return self.title[:20]
+        return self.title
 
 
 class Comment(models.Model):
-    text = models.TextField('Текст комментария')
-    post = models.ForeignKey(
-        Post,
-        verbose_name='Комментарий',
-        on_delete=models.SET_NULL,
-    )
     author = models.ForeignKey(
-        User,
-        verbose_name='Автор публикации',
-        on_delete=models.SET_NULL,
+        User, on_delete=models.CASCADE, null=True,
+        verbose_name='Автор комментария'
     )
-
-    class Meta:
-        verbose_name = 'комментарий'
-        verbose_name_plural = 'Комментарии'
-        related_name = 'comments'
-        ordering = ('-pub_date',)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE, null=True)
+    text = models.TextField(
+        verbose_name='Текст'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.text[:20]
+        return self.text
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for blogicum project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -32,9 +16,12 @@ urlpatterns = [
         CreateView.as_view(
             template_name='registration/registration_form.html',
             form_class=UserCreationForm,
-            success_url=reverse_lazy('pages:homepage'),
+            success_url=reverse_lazy('pages:index'),
         ),
         name='registration',
     ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
